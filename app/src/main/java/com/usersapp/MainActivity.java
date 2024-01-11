aa4312d6fe9794e46a47d8f719c19bf6d5f40efe
George <msy75ho@gmail.com>
2024-01-11 12:51:04 +0300

    made UI

--- app/src/main/java/com/usersapp/MainActivity.java
+++ app/src/main/java/com/usersapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.usersapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = findViewById(R.id.blur);
+        relativeLayout.getBackground().setAlpha(200);
     }
 }
---
+++ app/src/main/java/com/usersapp/Utils.java
@@ -0,0 +1,9 @@
+package com.usersapp;
+
+import com.google.gson.Gson;
+import okhttp3.*;
+import java.io.IOException;
+
+public class Utils {
+
+}
